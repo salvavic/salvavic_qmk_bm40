9978f7bfe3e6c912c421ecc1d1157a1674d347de
Salvador Vicente de Valenzuela <salvadorvicente@gmail.com>
2021-08-05 21:35:34 +0200

    Añadiendo cambios infinity

---
+++ rules.mk
@@ -0,0 +1,9 @@
+#rules
+EXTRAFLAGS += -flto
+
+# MOUSEKEY_ENABLE = yes # Mouse keys
+BOOTMAGIC_ENABLE = full
+# EXTRAKEY_ENABLE = yes # Audio control and System control
+TAP_DANCE_ENABLE = yes
+# RGB_MATRIX_ENABLE = yes
+# RGBLIGHT_ENABLE = yes
