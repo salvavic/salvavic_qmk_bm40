912839bdd5b95b0ff7f902ee7530ded2346790c6
Salvador Vicente de Valenzuela <salvador.vicente@qwi-ti.com>
2021-09-06 15:50:27 +0200

    cambio en rules

--- rules.mk
+++ rules.mk
@@ -2,7 +2,7 @@
 EXTRAFLAGS += -flto
 
 # MOUSEKEY_ENABLE = yes # Mouse keys
-BOOTMAGIC_ENABLE = full
+BOOTMAGIC_ENABLE = lite
 # EXTRAKEY_ENABLE = yes # Audio control and System control
 TAP_DANCE_ENABLE = yes
 # RGB_MATRIX_ENABLE = yes
