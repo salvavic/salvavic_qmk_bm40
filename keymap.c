025b1645d52f3ef81ecbcf4b52ef0776ebf76e68
Salvador Vicente de Valenzuela <salvador.vicente@qwi-ti.com>
2021-12-09 08:20:49 +0100

    Borrado y delete overrides

--- keymap.c
+++ keymap.c
@@ -157,7 +157,7 @@ const key_override_t bspace_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DELE
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
-    &delete_key_override,
+    &bspace_key_override,
     NULL // Null terminate the array of overrides!
 };
 
